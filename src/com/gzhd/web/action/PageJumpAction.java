fac196cb22107b84848ce26d2a964ee534e5b571
zhounan199252 <zhounan199252@163.com>
2016-06-23 16:07:45 +0800

    定时器

--- src/com/gzhd/web/action/PageJumpAction.java
+++ src/com/gzhd/web/action/PageJumpAction.java
@@ -10,6 +10,11 @@ import org.springframework.context.annotation.Scope;
 
 @Action(value = "pageJump", results = { 
 		@Result(name = "toHighFrequency", location = "/WEB-INF/pages/highFrequency/index.jsp"),
+		@Result(name = "toGd11x5", location = "/WEB-INF/pages/highFrequency/gd11x5.jsp"),
+		@Result(name = "toSsq", location = "/WEB-INF/pages/highFrequency/ssq.jsp"),
+		@Result(name = "toDlt", location = "/WEB-INF/pages/highFrequency/dlt.jsp"),
+		@Result(name = "toCqssc", location = "/WEB-INF/pages/highFrequency/cqssc.jsp"),
+		@Result(name = "toBjpk10", location = "/WEB-INF/pages/highFrequency/bjpk10.jsp"),
 		@Result(name = "toBankRecharge", location = "/WEB-INF/pages/highFrequency/recharge.jsp")
 	
 })
@@ -27,6 +32,37 @@ public class PageJumpAction  extends BaseAction<Object>{
 	 */
 	public String toHighFrequency() {
 		return "toHighFrequency";
+	}
+	
+	/**
+	 * 高频彩票
+	 */
+	public String toGd11x5() {
+		return "toGd11x5";
+	}
+	/**
+	 * 高频彩票
+	 */
+	public String toSsq() {
+		return "toSsq";
+	}
+	/**
+	 * 高频彩票
+	 */
+	public String toDlt() {
+		return "toDlt";
+	}
+	/**
+	 * 高频彩票
+	 */
+	public String toCqssc() {
+		return "toCqssc";
+	}
+	/**
+	 * 高频彩票
+	 */
+	public String toBjpk10() {
+		return "toBjpk10";
 	}
 	
 	/**
